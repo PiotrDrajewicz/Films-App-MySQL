33d256a894743d15bd91e947166e4434687192eb
unknown <drpiterek@gmail.com>
2023-08-28 22:50:34 +0200

    working on pupup animation

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -3,6 +3,7 @@
 import MoviePopup from './MoviePopup';
 import { useState } from "react";
 import { AnimatePresence, motion } from 'framer-motion';
+import { Scale } from 'lucide-react';
 
 interface MovieItemInterface {
     movieId: number;
@@ -25,13 +26,13 @@ const MovieItem: React.FC<MovieItemInterface> = ({movieId, title, poster_path, i
     }
     
     return (
-        <motion.div layoutId={`card-container-${movieId}`} className='movie-item-container' onClick={() => setChosenMovie(movieId)}>
+        <motion.div /*layoutId={`card-container-${movieId}`}*/ className='movie-item-container' onClick={() => setChosenMovie(movieId)}>
             {/* <motion.img className='movie-poster' src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="movie poster" onClick={showPopup} /> */}
-            <img /*initial={{width: 124, height: 185}}*/  className='movie-poster' src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="movie poster" onClick={showPopup} />
+            <motion.img  animate={{top: isActive ? '50%' : '0%', left: isActive ? '50%' : '0%', translateX: isActive ? '-50%' : '0%', translateY: isActive ? '-50%' : '0%', position: isActive ? 'fixed' : 'relative' }} transition={{duration: 2, delay: 0.15}} /*initial={{width: 124, height: 185}}*/  className={`movie-poster`} src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="movie poster" onClick={showPopup} />
             <AnimatePresence >
             {/* { isActive ? <MoviePopup title={title} poster_path={poster_path} setIsActive={setIsActive} overview={overview} movieId={movieId} /> : null } */}
             {/* {isActive && <motion.img initial={{width: 124, height: 185}} animate={{width: 300, height: 400}}  className='movie-popup' src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="movie poster" />} */}
-            { isActive ? <MoviePopup title={title} poster_path={poster_path} setIsActive={setIsActive} overview={overview} movieId={movieId} /> : null }
+            {/* { isActive ? <MoviePopup title={title} poster_path={poster_path} setIsActive={setIsActive} overview={overview} movieId={movieId} /> : null } */}
             </AnimatePresence >
         </motion.div>
     )
